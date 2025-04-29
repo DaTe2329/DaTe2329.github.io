0a2ddf7d2e76c2fcc408d3bbbaef5a5244962a23
Emdev <ecdk81@icloud.com>
2025-04-29 18:22:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const API_URL = 'https://api.football-data.org/v4/matches';
-const API_KEY = 'ea35b83b5e5e472bb5b09512025aa953';  
-
+const API_KEY = 'ea35b83b5e5e472bb5b09512025aa953';
 
 function fetchLiveMatches() {
   fetch(API_URL, {
     headers: {
-      'X-Auth-Token': API_KEY  
+      'X-Auth-Token': API_KEY  // Tu ajoutes le header avec la clé API
     }
   })
   .then(response => response.json())
   .then(data => {
-    const matches = data.matches; t
+    const matches = data.matches; // Récupération des matchs
     const matchList = document.querySelector('.match-list');
-    
     
     matches.forEach(match => {
       const matchCard = `
